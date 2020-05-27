9596bb8663285a902fa5b92d40e5d4a22289178b
Toby Dawson <tobydawson1@gmail.com>
2020-05-27 15:35:00 +0100

    expanded example

--- examples/useState/convertToClass.js
+++ examples/useState/convertToClass.js
@@ -21,3 +21,37 @@ render(
 
 // in order to add state so the button can only be clicked once you need to convert the function to a class like so:
 
+import React from 'react';
+import { render } from 'react-dom';
+
+class OneTimeButton extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  handleClick = () => {
+    this.props.onClick();
+    this.setState({ clicked: true});
+  }
+
+  render() {
+    return ( 
+      <button
+        onClick={this.handleClick}
+        disabled={this.state.clicked}
+      >
+        You Can Only Click Me Once
+      </button>
+    );
+  }
+}
+
+render(
+  <OneTimeButton />,
+  document.querySelector('#root')
+);
+
+// this is great and works but has changed a lot of code and made it longer. The second way of adding state would be to use hooks. Like so:
+
+
+
