ce7e7899d7cac015b2f245022119378050a01b3f
Toby Dawson <tobydawson1@gmail.com>
2020-06-05 12:35:32 +0100

    another way of doing it added

--- useState/backlight.js
+++ useState/backlight.js
@@ -43,6 +43,30 @@ function Room() {
 
 ReactDOM.render(<Room />, document.getElementById('root')); 
 
+
+// or ....
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+function Room() {
+  const [isLit, setLit] = useState(true);
+
+  return (
+    <div className={`room ${isLit ? "Lit" : "dark"}`}>
+      the room is {isLit ? 'lit' : 'dark'}
+      <br />
+      <button onClick={() => setLit(!isLit)}>
+        flip
+      </button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Room />, document.getElementById('root')); 
+
 // and the css
 
 html, body, #root, .room {
